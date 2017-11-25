f4235590718e87ef026b5200422cb0c28e044af0
Malrig <g-a-b@hotmail.co.uk>
2017-11-25 13:50:22 +0000

    Update account controller documentation

--- Expenses-Api/Identity/Controllers/AccountController.cs
+++ Expenses-Api/Identity/Controllers/AccountController.cs
@@ -26,14 +26,30 @@ namespace ExpensesApi.Identity.Controllers {
       //this.passwordHasher = passwordHasher;
     }
 
+    /// <summary>
+    /// Creates an account.
+    /// </summary>
+    /// <remarks> 
+    /// Sample request:
+    ///
+    ///     {
+    ///         "email": "test@dummymail.com",
+    ///         "password": "P4ssw0rd!"
+    ///     }
+    ///
+    /// </remarks>
+    /// <param name="newAccount">The new account to create.</param>
+    /// <returns></returns>
+    /// <response code="200">Returns a successful message.</response>
+    /// <response code="400">If the account fails validation checks.</response>  
     [HttpPost]
-    public async Task<IActionResult> Create([FromBody] AccountRegisterLogin model) {
+    public async Task<IActionResult> Create([FromBody] AccountRegisterLogin newAccount) {
       if (!ModelState.IsValid) {
         return BadRequest(ModelState.Values.SelectMany(v => v.Errors).Select(modelError => modelError.ErrorMessage).ToList());
       }
 
-      var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-      var result = await userManager.CreateAsync(user, model.Password);
+      var user = new ApplicationUser { UserName = newAccount.email, Email = newAccount.email };
+      var result = await userManager.CreateAsync(user, newAccount.password);
 
       if (!result.Succeeded) {
         return BadRequest(result.Errors.Select(x => x.Description).ToList());
@@ -44,13 +60,29 @@ namespace ExpensesApi.Identity.Controllers {
       return Ok();
     }
 
+    /// <summary>
+    /// Logs into an existing account.
+    /// </summary>
+    /// <remarks> 
+    /// Sample request:
+    ///
+    ///     {
+    ///         "email": "test@dummymail.com",
+    ///         "password": "P4ssw0rd!"
+    ///     }
+    ///
+    /// </remarks>
+    /// <param name="loginAccount">The details of the account to log in.</param>
+    /// <returns></returns>
+    /// <response code="200">Returns a successful message, along with a session cookie.</response>
+    /// <response code="400">If the account fails validation checks.</response>
     [HttpPost("login")]
-    public async Task<IActionResult> Login([FromBody] AccountRegisterLogin model) {
+    public async Task<IActionResult> Login([FromBody] AccountRegisterLogin loginAccount) {
       if (!ModelState.IsValid) {
         return BadRequest();
       }
 
-      var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: false, lockoutOnFailure: false);
+      var result = await signInManager.PasswordSignInAsync(loginAccount.email, loginAccount.password, isPersistent: false, lockoutOnFailure: false);
 
       if (!result.Succeeded) {
         return BadRequest();
--- Expenses-Api/Identity/Models/AccountRegisterLogin.cs
+++ Expenses-Api/Identity/Models/AccountRegisterLogin.cs
@@ -11,12 +11,12 @@ namespace ExpensesApi.Identity.Models {
     [Required]
     [EmailAddress]
     [Display(Name = "Email")]
-    public string Email { get; set; }
+    public string email { get; set; }
 
     [Required]
     [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
     [DataType(DataType.Password)]
     [Display(Name = "Password")]
-    public string Password { get; set; }
+    public string password { get; set; }
   }
 }
