b4190140611c9b40c8cdbfc5eb9d6bebf8c0ab34
Malrig <g-a-b@hotmail.co.uk>
2018-03-08 13:40:24 +0000

    Update build script

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -ev
 
 dotnet restore
 
-dotnet test .\Expenses-Api-Tests\Expenses-Api-Tests.csproj -c Release
-dotnet build .\Expenses-Api\Expenses-Api.csproj -c Release
+dotnet test ./Expenses-Api-Tests/Expenses-Api-Tests.csproj -c Release
+dotnet build ./Expenses-Api/Expenses-Api.csproj -c Release
