1f7763fe70f025baaaebcd0cab2db6b4726fd273
Malrig <g-a-b@hotmail.co.uk>
2018-03-08 13:21:53 +0000

    Playing with continuous integration

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Exit if any command fails
+set -ev
+
+dotnet restore
+
+dotnet test .\Expenses-Api-Tests\Expenses-Api-Tests.csproj -c Release
+dotnet build .\Expenses-Api\Expenses-Api.csproj -c Release
