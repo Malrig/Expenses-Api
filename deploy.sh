f1627e341d134f6e10534edcef17dfaad27d1654
Malrig <g-a-b@hotmail.co.uk>
2018-04-18 10:09:37 +0100

    Add port to deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 rsync -e 'ssh -p 22022' -r --delete-after --quiet $TRAVIS_BUILD_DIR/artifacts/ $SSH_LOGIN_USERNAME@malrig.homelinuxserver.org:/var/WebApps/Expenses-Api
 
-ssh $SSH_LOGIN_USERNAME@malrig.homelinuxserver.org << EOF
+ssh -p 22022 $SSH_LOGIN_USERNAME@malrig.homelinuxserver.org << EOF
   yes | cp -rf /var/WebApps/ConfigFiles/Expenses-Api/appsettings.json /var/WebApps/Expenses-Api/appsettings.json
   yes | cp -rf /var/WebApps/ConfigFiles/Expenses-Api/hosting.json /var/WebApps/Expenses-Api/hosting.json
   sudo systemctl restart kestrel-expenses-api.service
